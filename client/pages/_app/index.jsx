197b3c09d7e99007e2ba7c31f1678b33373bca7d
minghuiyang1998@gmail.com <minghuiyang1998@gmail.com>
2020-04-12 01:04:25 +0800

    init 20%

--- client/pages/_app/index.jsx
+++ client/pages/_app/index.jsx
@@ -1,16 +1,17 @@
 import globalStyle from '@/stylesheet/global.scss';
-import App, { Container } from 'next/app';
+import App from 'next/app';
 import React from 'react';
+import Header from 'components/Header';
 
 export default class cloudSQL extends App {
 
   render() {
-
+    const { Component } = this.props || {};
     return (
       <>
         <style jsx>{globalStyle}</style>
         <Header />
-        <Container />
+        <Component />
       </>
     );
   }
