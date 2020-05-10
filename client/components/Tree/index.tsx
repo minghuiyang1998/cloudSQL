fe76aef0533e0627456a2af63c88dba36b7c0285
minghuiyang1998@gmail.com <minghuiyang1998@gmail.com>
2020-05-10 17:12:36 +0800

    feat(): bugfix style sidebar

--- client/components/Tree/index.tsx
+++ client/components/Tree/index.tsx
@@ -2,20 +2,18 @@ import React from 'react';
 import style from './index.scss';
 
 const Tree = ({ data = [] }) => {
-  const renderBranch = (array) => {
-    return array.map((e) => {
-      const { children = [], name = '' } = e || {};
-      if (children.length > 0) {
-        return (
-          <ul key={name}>
-            {name}
-            {renderBranch(children)}
-          </ul>
-        );
-      }
-      return <li key={name}>{name}</li>;
-    });
-  };
+  const renderBranch = (array) => array.map((e) => {
+    const { children = [], name = '' } = e || {};
+    if (children.length > 0) {
+      return (
+        <ul key={name}>
+          {name}
+          {renderBranch(children)}
+        </ul>
+      );
+    }
+    return <li key={name}>{name}</li>;
+  });
 
   return (
     <ul className="tree">
